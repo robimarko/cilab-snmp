4f15aea042ce941b749ef898eea2102d8bc65dd4
Robert Marko <robimarko@gmail.com>
2019-04-30 00:42:02 +0200

    Make temperature script universal
    This removes the need for hardcoding

--- temperature.sh
+++ temperature.sh
@@ -1,7 +1,8 @@
 #!/usr/bin/env sh
 
-TEMPERATURE=$(cat /sys/bus/iio/devices/iio\:device0/in_temp_input)
-TEMPERATURE_OUT=$(cat /sys/bus/iio/devices/iio\:device1/in_temp_input)
-
-echo $TEMPERATURE
-echo $TEMPERATURE_OUT
+DEVICES=$(ls -d /sys/bus/iio/devices/*)
+arr=$(echo $DEVICES | tr " " "\n")
+for x in $arr
+do
+        echo $(cat ${x}/in_temp_input)
+done
